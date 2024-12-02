073c462dc217cfa10998ec4035a26b4b7e2e47b6
Diego <66350572+diegopereiracruz@users.noreply.github.com>
2024-12-01 22:33:02 -0400

    Update FletRouter.py

--- app/system/FletRouter.py
+++ app/system/FletRouter.py
@@ -2,10 +2,6 @@ import flet as ft
 
 # Pages
 from app.pages.home import home_page
-# from pages.library_page import LibraryPage
-# from pages.settings_page import SettingsPage
-# from pages.loading_page import LoadingPage
-# from pages.game_page import GameDetailPage  # Importe a página de detalhes do jogo
 
 class Router:
     
@@ -13,24 +9,21 @@ class Router:
         self.page = page
         self.ft = ft
         self.routes = {
+            # Add pages here
+            # 1. Create the page file in app/pages/   | Ex.: app/pages/settings.py
+            # 2. Import the page in this file         | Ex.: from app.pages.settings import settings_page
+            # 2. Put the path name of the page here   | Ex.: "/settings": settings_page(page),
             "/": home_page(page),
-            # Remova o conteúdo estático da biblioteca para garantir que ela seja recriada
-            # "/settings": SettingsPage(page),
-            # "/loading": LoadingPage(page),
-            # "/game_details": None,  # Este será atualizado dinamicamente com o ID do jogo
         }
         self.body = ft.Container(content=self.routes['/'])
 
     def route_change(self, route):
-        if route.route.startswith("/game_details"):
-            # Extraia o ID do jogo da rota e gere a página correspondente
-            # game_id = route.route.split("/")[-1]
-            # self.body.content = GameDetailPage(self.page, game_id)
+        if route.route.startswith("/example"):
+            # If the page's path starts with some name, do something.
             ...
             
-        elif route.route == "/library":
-            # Recrie a página da biblioteca sempre que a rota for /library
-            # self.body.content = LibraryPage(self.page)
+        elif route.route == "/example":
+            # If the page's path is equal some name, do something.
             ...
             
         else:
